9356d241fc41f6ea059f128587ee2331154f4728
szejzer <szejzer@gmail.com>
2017-03-04 08:04:50 +0100

    class type hints

--- Pro/ShopProduct.php
+++ Pro/ShopProduct.php
@@ -1,5 +1,7 @@
 <?php
 
+class Wrong {}
+
 class ShopProduct {
 	
 	public $title;
@@ -7,7 +9,7 @@ class ShopProduct {
 	public $producerFirstName;
 	public $price;
 	
-	function __construct($title,$producerFirstName,$producerMainName,$price){
+	public function __construct($title,$producerFirstName,$producerMainName,$price){
 		$this->title = $title;
 		$this->producerFirstName = $producerFirstName;
 		$this->producerMainName = $producerMainName;
@@ -15,22 +17,34 @@ class ShopProduct {
 		
 	}
 	
-	function getTitle(){
+	public function getTitle(){
 		return $this->title;
 	}
 	
-	function getPrice(){
+	public function getPrice(){
 		return $this->price;
 	}
 	
-	function getProducer(){
+	public function getProducer(){
 		
 		return "{$this->producerFirstName} ".
 				"{$this->producerMainName}";
 	}
 	
+	/*wymuszenie parametru poprzez użycie class type hints - pouczenia (mechanizm wymuszania typu)
+	 * nie działa dla typów prostych
+	 * */
+	public function write(ShopProduct $shopProduct){
+		$string = "{$shopProduct->getTitle()}: ".
+		"{$shopProduct->getProducer()}, ".
+		"{$shopProduct->getPrice()}";
+		
+		print $string;
+	}
+	
+	
 	//changing output from var_dump(), always return array
-	function __debugInfo(){
+	public function __debugInfo(){
 	
 		return array('title' => $this->title);
 	}
@@ -39,6 +53,30 @@ class ShopProduct {
 $product1 = new ShopProduct("Productivity 101","John", "Snow", 9.99);
 $product2 = new ShopProduct("Intermittent Fasting: Improving Cognitive Functions","Angelo","Mendoza", "14.99");
 
+$product1->write($product1);
+
+//$product1->write(new Wrong()); nie zadziała, bo nie zgadza się typ parametru
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print "<br><br>";
 var_dump($product1);
 
 print '<br><br>';
@@ -59,6 +97,11 @@ if(is_string($product1))
 	print "yes, it's a ".gettype($product1);
 else
 	print "naah, it's not a string, it's a(n) ".gettype($product1);
+
+	
+print "<br><br>";
+if(is_object($product2))
+	print '$product2 is an object';
 
 print "<br><br>";
 print "<br><br>";
