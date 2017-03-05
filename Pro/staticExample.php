630e7798aaef47bd6b8a298a0f313138707d0a34
szejzer <szejzer@gmail.com>
2017-03-05 05:44:31 +0100

    trying to repair trait

--- Pro/staticExample.php
+++ Pro/staticExample.php
@@ -1,12 +1,39 @@
 <?php
 
+/*
+ * static pierwsze, przed 
+ */
 class StaticExample{
 	
-	static public $nextID = 0;
+	static private $nextID = 1;
+	static public $greeting = "Siema!";
+	
+	public function __construct(){
+		print "New object with ID ".self::$nextID." has been created<br>";
+		self::$nextID++;
+	}
 	static public function sayHello(){
-		print "Siema";
+		print self::$greeting."<br>";
+	}
+	
+	static public function showLastObjectID(){
+		
+		if(StaticExample::$nextID == 1){
+			print "There are no objects<br>";
+		}
+		elseif (StaticExample::$nextID > 1){
+			print "Next object ID will be: ".self::$nextID."<br>";
+		}
+		
 	}
 }
 
 StaticExample::sayHello();
+$object = new StaticExample();
+$object = new StaticExample();
+StaticExample::showLastObjectID();
+StaticExample::$greeting = "Siemanson";
+StaticExample::sayHello();
+
+
 ?>
--- Shop/ShopProduct.php
+++ Shop/ShopProduct.php
@@ -1,19 +1,20 @@
 <?php
 
-class ShopProduct{
+class ShopProduct implements Chargeable{
 
 	private $title;
 	private $producerFirstName;
 	private $producerLastName;
 	protected $price;
 	private $discount = 0;
+	const AVAILABLE = 1;
+	const OUT_OF_STOCK = 0;
 
 	public function __construct($title, $producerFirstName, $producerLastName,$price){
 		$this->title = $title;
 		$this->producerFirstName = $producerFirstName;
 		$this->producerLastName = $producerLastName;
 		$this->price = $price;
-
 	}
 
 	public function getTitle(){
@@ -53,8 +54,6 @@ class ShopProduct{
 
 }
 
-
-
 class CdProduct extends ShopProduct{
 	private $playLength = 0;
 	
@@ -90,16 +89,86 @@ class BookProduct extends ShopProduct{
 	}
 }
 
+abstract class ShopProductWriter{
+	
+	protected $products = array();
+	
+	public function addProduct(Chargeable $shopProduct){
+		$this->products[] = $shopProduct;
+	}
+	
+	abstract public function write();
+}
+
+class XmlProductWriter extends ShopProductWriter{
+	public function write(){
+		$writer = new XMLWriter();
+		$writer->openMemory();
+		$writer->startDocument('1.0','UTF-8');
+		$writer->startElement("products");
+		foreach ($this->products as $shopProduct){
+			$writer->startElement("product");
+			$writer->writeAttribute("title", $shopProduct->getTitle());
+			$writer->startElement("summary");
+			$writer->text($shopProduct->getSummaryLine());
+			$writer->endElement();//element summary
+			$writer->endElement();//element product
+			
+		}
+		
+		$writer->endElement();
+		$writer->endDocument();
+		print $writer->flush();
+	}
+}
+
+class TextProductWriter extends ShopProductWriter {
+	public function write(){
+		$str = "PRODUCTS:<br>";
+		foreach($this->products as $shopProduct){
+			$str .= $shopProduct->getSummaryLine()."<br>";
+		}
+		print $str;
+	}
+}
+
+interface Chargeable{
+	public function getPrice();
+}
+
+
+
+
+
+
+
+
+//================================================================================================//
+
+print ShopProduct::AVAILABLE;
+print "<br>";
+
 $shopProduct = new ShopProduct("Brain", "Steven", "Kalper", 9.99);
 print "{$shopProduct->getSummaryLine()}"."<br>";
-
 
 $cdProduct = new CdProduct("Radio M", "Sir", "Małpa", 100, 56);
 print $cdProduct->getSummaryLine()."<br>";
 
-$bookProduct = new BookProduct("Ma Buk", "Anna", "Smith", 2.99, 300);
-print $bookProduct->getSummaryLine();
+$bookProduct = new BookProduct("Lolita", "Anna", "Smith", 2.99, 300);
+print $bookProduct->getSummaryLine()."<br>";
 
+$mywriter = new TextProductWriter();
+$mywriter->addProduct($shopProduct);
+$mywriter->addProduct($cdProduct);
+$mywriter->addProduct($bookProduct);
+$mywriter->write();
+
+//struktura xml widoczna w źródle strony
+$mywriter2 = new XmlProductWriter();
+$mywriter2->addProduct($shopProduct);
+$mywriter2->addProduct($cdProduct);
+$mywriter2->addProduct($bookProduct);
+$mywriter2->write();
 
 ?>
 
