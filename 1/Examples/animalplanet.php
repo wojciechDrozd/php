d2cb39ecabee5678278ce6c6f5af503fb13b535b
szejzer <szejzer@gmail.com>
2017-03-06 06:11:34 +0100

    animalplanet done

--- 1/Examples/animalplanet.php
+++ 1/Examples/animalplanet.php
@@ -39,20 +39,26 @@ abstract class Animal {
 	}
 	
 	public function __toString(){
-		return "<br><br>ID: ".$this->getID()."<br>Name: ".$this->getName()."<br>Age: ".$this->getAge();
+		return "ID: ".$this->getID()."<br>Name: ".$this->getName()."<br>Age: ".$this->getAge();
 	}
 	
 	abstract function move();
 	
 }
 
-class Bird extends Animal {
+class Bird extends Animal implements Flyable {
+	
+	use VoicesTrait;
 	
 	private $flyable;
 	
 	public function __construct($name,$age,$food,$flyable){
 		parent::__construct($name,$age,$food);
 		$this->flyable = $flyable;
+	}
+	
+	public function fly(){
+		echo $this->getName()." is flying like a free spirit";
 	}
 	
 	public function isFlyable(){
@@ -110,6 +116,24 @@ class Cat extends Animal implements Hunter,Lovable{
 	}
 }
 
+class Whale extends Animal {
+	
+	use VoicesTrait,TalentTrait{
+		TalentTrait::sing insteadof VoicesTrait;
+		VoicesTrait::sing as basicSing;
+	}
+
+		public function move(){
+		echo $this->getName()." is swimming in the ocean like a king of darkness";
+	}
+	
+	
+}
+
+interface Flyable {
+	public function fly();
+}
+
 interface Hunter{
 	public function hunt($tool);
 }
@@ -150,7 +174,6 @@ trait BasicNeedsUtilites{
 	
 }
 
-
 trait LoveUtilities{
 	
 	public function hug(){
@@ -160,65 +183,104 @@ trait LoveUtilities{
 	abstract function getName();
 }
 
-//**********Testing area******************************************************************************************//
+trait TalentTrait {
+	
+	public function sing(){
+		echo $this->getName(). " singing like Adele";
+	}
+	
+	abstract function getName();
+}
 
-$b1 = new Bird("Stefan", 21,"broccoli", true);
-print $b1;
-print "<br><br>";
-
-print $b1->move();
-print "<br><br>";
-
-print $b1->eat();
-print "<br><br>";
-
-$b2 = new Bird("Antek", 12, "fries", false);
-print $b2;
-print "<br><br>";
-
-print $b2->eat();
-print "<br><br>";
-
-$e1 =  new Eagle("Piti", 32, "onion", true);
-echo $e1->hunt("blender");
-print "<br><br>";
-
-echo $e1->prepareAttack();
-print "<br><br>";
+trait VoicesTrait {
+	
+	public function roar(){
+		echo $this->getName(). " is roaring in the forst";
+	}
+	
+	public function sing(){
+		echo $this->getName(). " is singing a song of it's people";
+	}
+	
+	abstract function getName();
+	
+	
+}
 
 
-echo $e1->eat();
-print "<br><br>";
 
-echo "Piti is a(n) ".get_class($e1);
-print "<br><br>";
 
-echo $e1->pee();
-print "<br><br>";
 
-$d1 = new Dog("Kali",3,"pizza");
-echo $d1->bark();
-print "<br><br>";
 
-echo $d1->hug();
-print "<br><br>";
+//Testing area******************************************************************************************//
 
-$c1 = new Cat("Lord", 7, "green peas");
-echo $c1->kiss();
-print "<br><br>";
+$bird = new Bird("Stefan", 21,"broccoli", true);
+print $bird;
+print "<br>";
 
-echo $c1->move();
-print "<br><br>";
+print $bird->move();
+print "<br>";
 
+print $bird->eat();
+print "<br>";
+
+$bird2 = new Bird("Antek", 12, "fries", false);
+print $bird2;
+print "<br>";
+
+print $bird2->eat();
+print "<br>";
+
+$eagle =  new Eagle("Piti", 32, "onion", true);
+echo $eagle->hunt("blender");
+print "<br>";
+
+echo $eagle->prepareAttack();
+print "<br>";
+
+echo $eagle->eat();
+print "<br>";
+
+echo $eagle->getName()." is a(n) ".get_class($eagle);
+print "<br>";
+
+echo $eagle->pee();
+print "<br>";
+
+$dog = new Dog("Kali",3,"pizza");
+echo $dog->bark();
+print "<br>";
+
+echo $dog->hug();
+print "<br>";
+
+$cat = new Cat("Lord", 7, "green peas");
+echo $cat->kiss();
+print "<br>";
+
+echo $cat->move();
+print "<br>";
 
 echo Animal::$animalCount;
-print "<br><br>";
+print "<br>";
 echo Bird::$animalCount;
-print "<br><br>";
+print "<br>";
 echo Eagle::$animalCount;
-print "<br><br>";
+print "<br>";
+
+$whale = new Whale("Nikita", 102, "popcorn");
+echo $whale->sing();
+print "<br>";
+echo $whale->basicSing();
+print "<br>";
+
+
+
 echo Dog::$animalCount;
-print "<br><br>";
+print "<br>";
+
+echo $whale::$animalCount;
+print "<br>";
 
 /* $animals = array();
 
@@ -235,8 +297,7 @@ print "<br><br>";
 print_r            ( Animal::$animals); //też działa, spacje bez znaczenia 
 print "<br><br>";
 
-$number = 7;
-var_dump($number);
+
 
 
 ?>
