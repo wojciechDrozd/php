149b8aae5e49431e4d1a32e18a34cc16d6f92173
szejzer <szejzer@gmail.com>
2017-03-13 20:07:43 +0100

    update

---
+++ 1/mmTuts/loops.php
@@ -0,0 +1,7 @@
+<?php
+
+$counter = 0;
+while($counter <10){
+	echo "yo<br>";
+}																																																																																																																																																																																																																																																																																																													
+?>																																																																																																																																																																																																																																																																																																																																																																									
