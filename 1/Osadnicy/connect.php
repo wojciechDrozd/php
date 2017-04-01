2109d977e9cc241047db61e1a0c5cd5d1dcf3a1b
szejzer <szejzer@gmail.com>
2017-04-01 09:10:19 +0200

    update

---
+++ 1/Osadnicy/connect.php
@@ -0,0 +1,9 @@
+<?php
+
+//ogólny plik zawierający namiary na bazę a nie na tabelę
+//tabele bedą określane w konkretych plikach
+$host = "localhost";
+$db_user = "root";
+$db_password= "";
+$db_name = "Osadnicy";
+?>
