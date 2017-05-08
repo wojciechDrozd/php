5189a8123c7e7049f7d527fcf27d4b48dcc92576
szejzer <szejzer@gmail.com>
2017-05-08 07:15:48 +0200

    wiggle.com basic scanning done

--- vardump/wiggle.com/wiggle.php
+++ vardump/wiggle.com/wiggle.php
@@ -1,24 +1,19 @@
 <?php 
 
 require_once 'scanner.php';
-
 $scanner = new Scanner('http://www.wiggle.co.uk/');
+$catReg = '@href="(http://www.wiggle.co.uk/([^/]+)/)" class="bem-menu__item-link@';
+$scanner->setCatReg($catReg);
+$nextPageReg ='@bem-paginator__link--selected">\d+</span>.+?(?!=<)<a class="bem-paginator__link js-nav-ajax" data-ajax="[^"]+" href="([^"]+?)">\d+</a>@s';
+$scanner->setNextPageReg($nextPageReg);
+$productReg ='@bem-product-thumb__name--grid"\s+href="([^"]+?)"\s+data-ga-label="[^"]+"\s+
+		data-ga-action="Product\sTitle">([^<]+?)</a>\s+
+		<div\sclass="bem-product-price--grid">\s+<span\sclass="bem-product-price__unit--grid">\D*(\d+.\d+)(?:\D*(\d+\.\d+))?</span>@sx';
+$scanner->setProductReg($productReg);
 
 
-$url = "http://www.wiggle.co.uk/";
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, -1);
-$urlContent = curl_exec($ch);
-
-//categories regex
-$regex = '@href="(http://www.wiggle.co.uk/([^/]+)/)" class="bem-menu__item-link@';
-preg_match_all($regex,$urlContent,$matches);
-
-
-//echo "<pre>",print_r($matches),"</pre>";
-
+$matches = $scanner->getCategories();
 $categoriesNamesLinks = array();
-
 
 for ($i=0; $i<count($matches[1]); $i++){
 	$key = $matches[2][$i];
@@ -27,35 +22,38 @@ for ($i=0; $i<count($matches[1]); $i++){
 	$categoriesNamesLinks[$key] = $value;
 }
 ksort($categoriesNamesLinks);
+
+//header of the category names and links table
 $table = '<table class="table-bordered table-striped">
 		<tr>
 			<th>Category</th>
 			<th>Link</th>
-			<th>Show products</th>
 		</tr>';
+
+//populating category names and links table
 foreach($categoriesNamesLinks as $key => $value){
 	$table .= '<tr>
 				 <td>'.$key.'</td>
 				 <td><a href="'.$value.'">'.$value.'</a></td>
 			   </tr>';
 }
-
 $table .= '</table>';
+//print $table;
 
-
-//link do pierwszej strony z listą produktów w podkategorii
-$firstPage ='http://www.wiggle.com/accessories/?o=9';
+//get random category
+$randKey = array_rand($categoriesNamesLinks);
+$firstPage =$categoriesNamesLinks[$randKey];
 
 $url = $firstPage;
+//array of links to all 'next pages' in that subcategory
 $urls = array();
 $urls[] = $firstPage;
 
-//wersja uproszczona bez while
-
-for ($i=0;$i <5;$i++){
-	$url = nextPage($url);
-	$urls[] = $url;
-	
+//simplified version without while (less products, first 5 pages)
+for ($i=0;$i <2;$i++){
+	$url = $scanner->getNextPageLink($url);
+	if(!in_array($url,$urls))
+		$urls[] = $url;
 }
 
 
@@ -64,90 +62,64 @@ for ($i=0;$i <5;$i++){
  * kolejnej strony w podkategorii dopóki nie zaczynają się powtarzać
  * za duże na testowanie na localhost
  */
-/* while(nextPage($url)){
-	$url = nextPage($url);
-	if(in_array($url,$urls)){
-		break;
+/* while($scanner->getNextPageLink($url) != null){
+	$url = $scanner->getNextPageLink($url);
+	if(!in_array($url,$urls)){
+		$urls[] = $url;
 	}
-	$urls[] = $url;
-}
- */
+} */
+
  
-//fetching products from subcategory view
-$productRegex ='@bem-product-thumb__name--grid"\s+href="([^"]+?)"\s+data-ga-label="[^"]+"\s+
-		data-ga-action="Product\sTitle">([^<]+?)</a>\s+
-		<div\sclass="bem-product-price--grid">\s+<span\sclass="bem-product-price__unit--grid">€(\d+.\d+)</span>@sx';
+ array_unique($urls);
+ //echo "<pre>",print_r($urls),"</pre>";
 
-//populate array product name => price
+
+//populate array of strings: product name|price|link
 $prices = array();
 foreach ($urls as $item){
-	$ch = curl_init($item);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER,-1);
-	$urlContent = curl_exec($ch);
-	curl_close($ch);
-	preg_match_all($productRegex, $urlContent,$matches);
+	$matches = $scanner->getProducts($item);
 	for ($i=0;$i<count($matches[0]);$i++){
-		$prices["{$matches[2][$i]}"] = $matches[3][$i];
-	}
-			
-}
-
-ksort($prices);
-
-foreach ($prices as $key => $value){
-	//echo $key," ",$value,"<br/>";
-}
-
-//populate array of strings: link|product name|price
-$prices = array();
-foreach ($urls as $item){
-	$ch = curl_init($item);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER,-1);
-	$urlContent = curl_exec($ch);
-	curl_close($ch);
-	preg_match_all($productRegex, $urlContent,$matches);
-	for ($i=0;$i<count($matches[0]);$i++){
-		
-		$link = $matches[1][$i];
 		$productName = $matches[2][$i];
 		$price = $matches[3][$i];
-		$priceString = $link."|".$productName."|".$price;
-		array_push($prices,$priceString);
+		$link = $matches[1][$i];
+		$priceString = $productName."|".$price."|".$link;
+		if(!in_array($priceString, $prices))
+			array_push($prices,$priceString);
 	}
 
 }
 
-foreach($prices as $item){
-	//echo $item,"<br/>";
-}
+sort($prices);
+//array_unique($prices);
 
-$exploded = explode("|",$prices[0]);
-echo "<pre>",print_r($exploded),"<pre/>";
-
-//header of the link-product-price table
-$data = '<table>
+//header of the ppl table
+$data = '<table class="table-bordered table-responsive table-striped" id="ppl-table">
 		<tr>
-		<th>Link</th>
+			<th colspan="4" id="myth">
+			'.$randKey.'
+			</th>
+		</tr>
+		<tr>
+		<th>No.</th>
 		<th>Product name</th>
 		<th>Price</th>
+		<th>Link</th>
 		</tr>';
 
-//get url to the next page
-function nextPage($url){
-	$ch = curl_init($url);
-	curl_setopt($ch,CURLOPT_RETURNTRANSFER,-1);
-	$urlContent = curl_exec($ch);
-	curl_close($ch);
-	$nextPageLinkReg ='@bem-paginator__link--selected">\d+</span>.+?(?!=<)<a class="bem-paginator__link js-nav-ajax" data-ajax="[^"]+" href="([^"]+?)">\d+</a>@s';
-	preg_match_all($nextPageLinkReg, $urlContent, $matches);
-	$matches[1] = array_unique($matches[1]);
-	$nextPage =$matches[1][0];
-	$nextPage = html_entity_decode($matches[1][0]);
-	return $nextPage;
-	
+//populating ppl table
+for($i=0; $i<count($prices); $i++){
+	$exploded = explode("|",$prices[$i]);
+	$data .= '<tr>
+				<td>'.($i+1).'</td>
+				<td>'.$exploded[0].'</td>
+				<td>'.$exploded[1].'</td>
+				<td><a href="'.$exploded[2].'" target="_blank">Wiggle.com</a></td>
+			</tr>';
 }
 
+// lpp table end tag
+$data .= '</table>';
 
-
+echo $data;
 
 ?>
