8924ee53e5024bf0800a2044ac1616ab76950109
szejzer <szejzer@gmail.com>
2017-06-16 22:25:38 +0200

    task13.php

--- vardump/tasks/task13.php
+++ vardump/tasks/task13.php
@@ -9,13 +9,35 @@ Title => I like trains
 Url => https://www.example.com/
 */
 
+//get url content
+function getUrlContent($url){
+	$ch = curl_init($url);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	$content= curl_exec($ch);
+	curl_close($ch);
+	return $content;
+}
+
 $url ='http://it-supplier.co.uk/computers/all-in-one-pcs';
 $content = getUrlContent($url);
 $nextPageRegex ='%class="next i-next" href="([^"]+)" title="Next%';
 $productRegex ='%class="box\s*item\s*last">.*href="([^"]+)"\s*title="([^"]+)"\s*class="product-im%Usix';
 $products = array();
-$coutner = 0;
+$counter = 0;
 
+//fetch product from the first page
+preg_match_all($productRegex, $content,$matches);
+for ($i =0; $i < count($matches[0]); $i++){
+	$product = array(
+			'Title' => $matches[2][$i],
+			'Url' => $matches[1][$i],
+	);
+	
+	$products[] = $product;
+	$counter++;
+}
+
+//fetch products from the naxt pages
 while(preg_match($nextPageRegex,$content,$match)){
 	
 	$content = getUrlContent($match[1]);
@@ -28,21 +50,18 @@ while(preg_match($nextPageRegex,$content,$match)){
 		
 		$products[] = $product;
 		$counter++;
-		echo "Product $counter";
-		print_r($product);
 	}
-	
-	
-	
+
 }
 
 
 
-function getUrlContent($url){
-	$ch = curl_init($url);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	$content= curl_exec($ch);
-	curl_close($ch);
-	return $content;
-}
+print_r($products);
+echo "\n$counter products fetched\n";
+
+
+
+
+
+
 
