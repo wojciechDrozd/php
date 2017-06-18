4e6c31175bc7545b38d25402ef664755a4990b20
szejzer <szejzer@gmail.com>
2017-06-18 12:35:39 +0200

    task15

--- vardump/tasks/task15.php
+++ vardump/tasks/task15.php
@@ -1,30 +1,101 @@
 <?php
 
 
-$url = 'http://www.tredz.co.uk/road-bikes';
-$baseUrl ='http://www.tredz.co.uk';
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
-$response = curl_exec($ch);
-curl_close($ch);
+/*
+ * Fetch all road bikes (Name, link to product, price, frame material).
+ * Extract bikes with aluminium frame.
+ * Results save to csv file with bikes sorted by price in ascending order
+ */
 
+ini_set('max_execution_time', 0);
+
+function getUrlContent($url){
+	
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL,$url);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+	$content = curl_exec($ch);
+	curl_close($ch);
+	return $content;
+	
+	
+}
+
+function fetchProduct($matches){
+	
+}
+
+$baseUrl ='http://www.tredz.co.uk';
+$firstPageUrl = 'http://www.tredz.co.uk/road-bikes';
+$nextPageRegex = '%rel="next"[\s\t]+href="([^"]+)"%Usix';
 $productRegex ='%class="ga"[\s\t]*href="([^"]+)">.*
 				ellipsis[\s\t]*">([^<]+)<.*price">([^<]+)<%Usix';
 
-preg_match_all($productRegex,$response,$matches);
+$frameRegex = '%<b>Frame</b>:([^<]+)<br>%Usi';
 
+$content = getUrlContent($firstPageUrl);
+
+
+
+//array for products fetched from all pages
 $products = array();
+
+//fetch products from the first page
+preg_match_all($productRegex,$content,$matches);
+
 
 for($i =0; $i <count($matches[0]); $i++){
 	$product = array(
-			'url' => $baseUrl.trim($matches[1][$i]),
 			'name' => trim($matches[2][$i]),
-			'price' => str_replace("Now £","",trim($matches[3][$i])),
+			'url' => $baseUrl.trim($matches[1][$i]),
+			//'price' => str_replace("Now £","",trim($matches[3][$i])),
+			//'price1' => trim($matches[3][$i]),
+			'price' => preg_replace("/.*?((?:\d+,)*\d{2,3}(?:\.\d{2})?).*/i","$1",trim($matches[3][$i])),
 			
 	);
-				
+	
+	$itemContent = getUrlContent($baseUrl.trim($matches[1][$i]));
+	preg_match($frameRegex, $itemContent,$match);
+	$product['frame'] = trim($match[1]);
+	
+	
 	$products[] = $product;
 }
 
+//fetch products from the next pages
+while(preg_match($nextPageRegex,$content,$match)){
+	$content = getUrlContent($match[1]);
+	
+	preg_match_all($productRegex,$content,$matches);
+	for($i =0; $i <count($matches[0]); $i++){
+		$product = array(
+				'name' => trim($matches[2][$i]),
+				'url' => $baseUrl.trim($matches[1][$i]),
+				//'price' => str_replace("Now £","",trim($matches[3][$i])),
+				//'price1' => trim($matches[3][$i]),
+				'price' => preg_replace("/.*?((?:\d+,)*\d{2,3}(?:\.\d{2})?).*/i","$1",trim($matches[3][$i])),
+				
+		);
+		
+		$itemContent = getUrlContent($baseUrl.trim($matches[1][$i]));
+		preg_match($frameRegex, $itemContent,$match);
+		$product['frame'] = trim($match[1]);
+		
+		
+		$products[] = $product;
+		
+		echo"<pre>",print_r($product),"</pre>";
+	}
+	
+}
 
-print_r($products);
+
+
+//file pointer
+$fp = fopen('task15.csv','w');
+foreach($products as $product){
+	fputcsv($fp,$product);
+}
+fclose($fp);
+
+//echo"<pre>",var_dump($products),"<pre>";
